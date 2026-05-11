9186a789e68f436315fec44741e8b3be8f865461
wcsmomo02 <yangdengkui01@gmail.com>
2026-05-11 17:11:30 +0800

    Support Pages asset binding

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,5 +1,8 @@
 import { handleRequest } from '../worker/index.js'
 
 export async function onRequest({ request, env }) {
-  return handleRequest(request, env)
+  return handleRequest(request, {
+    ...env,
+    SITE_ASSETS: env.SITE_ASSETS || env.ASSETS,
+  })
 }
